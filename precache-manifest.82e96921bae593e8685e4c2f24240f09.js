b669224e469a4945a563348d578e37bd961d4893
FaisalMashuri <faisal.mashuri18143@student.unsika.ac.id>
2020-05-07 17:18:35 +0700

    Updates

---
+++ precache-manifest.82e96921bae593e8685e4c2f24240f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f6d60aeef1905ab6b63d4923003f34",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "15a9b5946ddef8ef0d79",
+    "url": "/covid19/static/css/main.7f72383f.chunk.css"
+  },
+  {
+    "revision": "e0cc111a91f0c74fbc31",
+    "url": "/covid19/static/js/2.d9b62dba.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.d9b62dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a9b5946ddef8ef0d79",
+    "url": "/covid19/static/js/main.5564bdbf.chunk.js"
+  },
+  {
+    "revision": "080958ad2f3d2ab682fd",
+    "url": "/covid19/static/js/runtime-main.6dbe199c.js"
+  }
+]);
